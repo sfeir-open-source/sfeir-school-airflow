2f56490d02d5d9f54a8f5fda3f63601e4829facc
François Courmont <courmont.f@sfeir.com>
2022-05-10 11:36:37 +0200

    fix: reorder

--- docs/scripts/slides.js
+++ docs/scripts/slides.js
@@ -11,7 +11,6 @@ function introSlides() {
   return [
     '01-airflow-intro/00-TITLE.md',
     '01-airflow-intro/01-intro-airflow.md',
-    '01-airflow-intro/01-intro-orchestrator.md',
     '01-airflow-intro/02-airflow-ui.md',
     '01-airflow-intro/03-history.md',
     '01-airflow-intro/04-intro-orchestrator.md',
